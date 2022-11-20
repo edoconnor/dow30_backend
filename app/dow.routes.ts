4abf1bb07d6c25746e1a7fc8ab5531c4c9120e73
Ed O'Connor <ed@oconnor.app>
2022-11-20 11:30:43 -0500

    edit

--- app/dow.routes.ts
+++ app/dow.routes.ts
@@ -1,11 +1,11 @@
-const express = require('express')
-const mongodb = require('mongodb')
+import express from "express";
+import mongodb from "mongodb";
 import { collections } from "./database";
 
 export const dowRouter = express.Router();
 dowRouter.use(express.json());
 
-dowRouter.get("/", async (req, res) => {
+dowRouter.get("/", async (req: any, res: any) => {
   try {
     const dow30 = await collections.dow30.find({}).toArray();
     res.status(200).send(dow30);
